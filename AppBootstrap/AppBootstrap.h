3f2e32545a61e200f7256943f3e5937da4ebced3
Yaming <yamingd@qq.com>
2014-10-30 16:16:24 +0800

    Initial Commit

---
+++ AppBootstrap/AppBootstrap.h
@@ -0,0 +1,13 @@
+//
+//  AppBootstrap.h
+//  AppBootstrap
+//
+//  Created by Yaming on 10/30/14.
+//  Copyright (c) 2014 whosbean.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppBootstrap : NSObject
+
+@end
---
+++ AppBootstrap/AppBootstrap.m
@@ -0,0 +1,13 @@
+//
+//  AppBootstrap.m
+//  AppBootstrap
+//
+//  Created by Yaming on 10/30/14.
+//  Copyright (c) 2014 whosbean.com. All rights reserved.
+//
+
+#import "AppBootstrap.h"
+
+@implementation AppBootstrap
+
+@end
